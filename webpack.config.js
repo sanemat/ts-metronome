c734a9624317bf06e4b566f3be7ee9c4fc7f233a
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-01-19 20:33:18 +0700

    chore(webpack): setting

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const { resolve } = require('path')
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+const config = {
+    entry: {
+        main: resolve('./src/index.ts')  },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: ['awesome-typescript-loader?module=es6'],
+                exclude: [/node_modules/]
+            },
+            {
+                test: /\.js$/,
+                loader: 'source-map-loader',
+                enforce: 'pre'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx']
+    }
+}
+
+module.exports = config
