d43b54768d290944e0272eb89df95bc3593ed091
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-01-23 13:26:50 +0700

    chore(css): use css

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ const config = {
             options: { minimize: !isDevelopment }
           }
         ]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
