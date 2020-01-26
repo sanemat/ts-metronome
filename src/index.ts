802b304f1d17d83beb22eed562bdcffa77feea47
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-01-26 19:25:18 +0700

    chore(playing): toggle

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,23 @@ import "./index.css";
 
 // @ts-ignore
 let audioContext = null;
+let playing = false;
 
 function init() {
   audioContext = new AudioContext();
 }
 
-function play() {
-  console.log("played");
+function togglePlayer() {
+  if (playing) {
+    console.log("Stop!");
+    playing = false;
+  } else {
+    console.log("Run!");
+    playing = true;
+  }
 }
 
 document.addEventListener("DOMContentLoaded", init);
-document.querySelector(".header--control")?.addEventListener("click", play);
+document
+  .querySelector(".header--control")
+  ?.addEventListener("click", togglePlayer);
