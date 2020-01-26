7eeb7c93e0438faaf61fcbcbe4710edb7f741dad
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-01-26 20:12:29 +0700

    chore(osc): run oscillator

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,30 @@
 import "./index.css";
 
-// @ts-ignore
-let audioContext = null;
+let audioContext: AudioContext = null;
 let playing = false;
+let beatNumber = 16;
 
 function init() {
   audioContext = new AudioContext();
 }
 
+function tick(beatNumber: number) {
+  // create an oscillator
+  const osc = audioContext.createOscillator();
+  osc.connect(audioContext.destination);
+  if (beatNumber % 16 === 0) {
+    // beat 0 == high pitch
+    osc.frequency.setValueAtTime(880, audioContext.currentTime);
+  } else if (beatNumber % 4 === 0) {
+    // quarter notes = medium pitch
+    osc.frequency.setValueAtTime(440, audioContext.currentTime);
+    // other 16th notes = low pitch
+  } else {
+    osc.frequency.setValueAtTime(220, audioContext.currentTime);
+  }
+  osc.start(audioContext.currentTime);
+  osc.stop(audioContext.currentTime + 50);
+}
 function togglePlayer() {
   if (playing) {
     console.log("Stop!");
@@ -19,6 +36,7 @@ function togglePlayer() {
   } else {
     console.log("Run!");
     playing = true;
+    tick(beatNumber);
     const button = document.querySelector(".header--control");
     if (button) {
       button.innerHTML = "Stop";
