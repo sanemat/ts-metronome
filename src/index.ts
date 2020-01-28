90db5f818cefb56d09b4eb14c7ebf647bf10a95b
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-01-28 20:00:38 +0700

    feat(coin): coin

--- src/index.ts
+++ src/index.ts
@@ -4,29 +4,56 @@ let audioContext: AudioContext = null;
 let playing = false;
 let schedulerId: number = null;
 let nextNoteTime: number = 0;
-let scheduleAheadTime: number = 0.1;// 100ms ahead
+let scheduleAheadTime: number = 0.1; // 100ms ahead
+let tempo = 240;
 
 function init() {
   audioContext = new AudioContext();
 }
 
-function scheduleNote(time: number) {
-  // create an oscillator
-  const oscillator = audioContext.createOscillator();
-  oscillator.connect(audioContext.destination);
-  oscillator.frequency.setValueAtTime(880, time);
-  oscillator.start(time);
-  oscillator.stop(time + 0.05);
+// https://qiita.com/mohayonao/items/c506f7ddcaac63694eb9
+function mtof(midi: number): number {
+  return 440 * Math.pow(2, (midi - 69) / 12);
 }
 
-function nextNote() {
-  nextNoteTime += 0.25;// 120BPM
+// https://qiita.com/mohayonao/items/c506f7ddcaac63694eb9
+function tdur(tempo: number, length: number): number {
+  return (60 / tempo) * (4 / length);
+}
+
+function coin(destination: AudioDestinationNode, time: number, tempo: number) {
+  const t0 = time;
+  const t1 = t0 + tdur(tempo, 16);
+  const t2 = t0 + tdur(tempo, 4) * 3;
+  const si = mtof(83);
+  const mi = mtof(88);
+  const context = destination.context;
+  const oscillator = audioContext.createOscillator();
+  const gain = context.createGain();
+  oscillator.type = "square";
+  oscillator.frequency.setValueAtTime(si, t0);
+  oscillator.frequency.setValueAtTime(mi, t1);
+  oscillator.start(t0);
+  oscillator.stop(t2);
+  oscillator.connect(gain);
+  gain.gain.setValueAtTime(0.5, t0);
+  gain.gain.setValueAtTime(0.5, t1);
+  gain.gain.linearRampToValueAtTime(0, t2);
+  gain.connect(destination);
+}
+
+function scheduleNote(time: number, tempo: number) {
+  coin(audioContext.destination, time, tempo);
+}
+
+function nextNote(tempo: number) {
+  nextNoteTime += 60 / tempo;
 }
 
 function schedule() {
   while (nextNoteTime < audioContext.currentTime + scheduleAheadTime) {
-    scheduleNote(nextNoteTime);
-    nextNote();
+    scheduleNote(nextNoteTime, tempo);
+    nextNote(tempo);
   }
 }
 
