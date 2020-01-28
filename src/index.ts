d353d63a6adce904af9752e0ea6baf6e71f5f853
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-01-28 17:55:19 +0700

    feat(scheduler): use scheduler

--- src/index.ts
+++ src/index.ts
@@ -2,39 +2,39 @@ import "./index.css";
 
 let audioContext: AudioContext = null;
 let playing = false;
-let beatNumber = 16;
+let schedulerId: number = null;
+let nextNoteTime: number = 0;
+let scheduleAheadTime: number = 0.1;// 100ms ahead
 
 function init() {
   audioContext = new AudioContext();
 }
 
-function tick(beatNumber: number) {
-  if (!playing) {
-    return;
-  }
+function scheduleNote(time: number) {
   // create an oscillator
   const oscillator = audioContext.createOscillator();
   oscillator.connect(audioContext.destination);
-  if (beatNumber % 16 === 0) {
-    // beat 0 == high pitch
-    oscillator.frequency.setValueAtTime(880, audioContext.currentTime);
-  } else if (beatNumber % 4 === 0) {
-    // quarter notes = medium pitch
-    oscillator.frequency.setValueAtTime(440, audioContext.currentTime);
-    // other 16th notes = low pitch
-  } else {
-    oscillator.frequency.setValueAtTime(220, audioContext.currentTime);
-  }
-  oscillator.start(audioContext.currentTime);
-  oscillator.stop(audioContext.currentTime + 0.05);
-
-  // 120BPM
-  setTimeout(tick, 500, [beatNumber]);
+  oscillator.frequency.setValueAtTime(880, time);
+  oscillator.start(time);
+  oscillator.stop(time + 0.05);
 }
+
+function nextNote() {
+  nextNoteTime += 0.25;// 120BPM
+}
+
+function schedule() {
+  while (nextNoteTime < audioContext.currentTime + scheduleAheadTime) {
+    scheduleNote(nextNoteTime);
+    nextNote();
+  }
+}
+
 function togglePlayer() {
   if (playing) {
     console.log("Stop!");
     playing = false;
+    clearInterval(schedulerId);
     const button = document.querySelector(".header--control");
     if (button) {
       button.innerHTML = "Play";
@@ -42,7 +42,7 @@ function togglePlayer() {
   } else {
     console.log("Run!");
     playing = true;
-    tick(beatNumber);
+    schedulerId = setInterval(schedule, 100);
     const button = document.querySelector(".header--control");
     if (button) {
       button.innerHTML = "Stop";
